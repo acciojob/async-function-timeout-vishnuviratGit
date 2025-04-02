bc8fb10fac4047661eb5a3ac1721f2a46458cd68
vishnuviratGit <vishnuvirat564@gmail.com>
2025-04-02 16:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const textInput=document.getElementById("text");
+const numberInput=document.getElementById("delay");
+const btn=document.getElementById("btn");
+const outputDiv=document.getElementById("output");
+btn.addEventListener("click", display);
+async function display() {
+	  try{
+		 let delay=parseInt(numberInput.value);
+		  await new Promise(resolve=>{
+			  setTimeout(resolve,delay);
+		  });
+		  dataDisplay(textInput.value);
+	  }
+	  catch(error){
+		  console.log("Error occured", error);
+	  }
+}
+function dataDisplay(data) {
+	 outputDiv.innerHTML=`${data}`;
+}
